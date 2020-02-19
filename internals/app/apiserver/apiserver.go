675d347b9a06969551096432b002d8a3ea825eea
free.dev <tima.75.com@gmail.com>
2020-02-20 00:03:44 +0600

    added css and html files

--- internals/app/apiserver/apiserver.go
+++ internals/app/apiserver/apiserver.go
@@ -1,8 +1,11 @@
 package apiserver
 
 import (
+	"bufio"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -16,6 +19,11 @@ type APIServer struct {
 }
 
 var htmlTemplate = template.Must(template.ParseFiles("./front/mainPage.html"))
+
+const pathToStandardFile = "./internals/app/apiserver/standard.txt"
+
+// const pathToShadowFile = ""
+// const pathToThinkyFile = ""
 
 //New ...
 func New(config *Config) *APIServer {
@@ -46,12 +54,71 @@ func (s *APIServer) configureLogger() error {
 }
 
 func (s *APIServer) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
-
+	s.router.HandleFunc("/", s.handleHello())
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		htmlTemplate.Execute(w, nil)
+		switch r.Method {
+		case "POST":
+			// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+			if err := r.ParseForm(); err != nil {
+				fmt.Fprintf(w, "ParseForm() err: %v", err)
+				return
+			}
+			text := r.FormValue("text")
+			ascii := r.FormValue("ascii")
+
+			if ascii == "default" {
+				value := openFile(text, pathToStandardFile)
+				fmt.Print(value)
+				myvar := map[string]interface{}{"MyVar": value}
+				htmlTemplate.Execute(w, myvar)
+			}
+		default:
+			htmlTemplate.Execute(w, nil)
+		}
 	}
+}
+
+func openFile(enteredValue string, fileName string) string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "cant find file " + fileName
+	}
+
+	var asciiArray []string
+	i := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		asciiArray = append(asciiArray, scanner.Text())
+		i++
+	}
+
+	if err1 := scanner.Err(); err1 != nil {
+		fmt.Println(err1.Error())
+		return "cant read file " + fileName
+	}
+	file.Close()
+	return printASCII("", asciiArray, enteredValue)
+}
+
+func printASCII(value string, asciiArray []string, enteredValue string) string {
+	var wordIndex int
+	for i := 0; i <= 7; i++ {
+		for j := range enteredValue {
+			if enteredValue[j] == 92 && enteredValue[j+1] == 110 {
+				wordIndex = j
+				break
+			} else {
+				value += asciiArray[(int(enteredValue[j])-32)*9+1+i]
+			}
+		}
+		value += "\n"
+	}
+	if wordIndex != 0 {
+		printASCII(value, asciiArray, enteredValue[wordIndex+2:len(enteredValue)])
+	}
+	return value
 }
